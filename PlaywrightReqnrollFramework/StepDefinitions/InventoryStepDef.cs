9910f425faaa7b7d5123fd8ba1864308f06e70ff
suban5 <subandhyako55@gmail.com>
2025-05-27 00:31:34 +0545

    Refactored POM model to use Dependency Injection

--- PlaywrightReqnrollFramework/StepDefinitions/InventoryStepDef.cs
+++ PlaywrightReqnrollFramework/StepDefinitions/InventoryStepDef.cs
@@ -9,21 +9,10 @@ namespace PlaywrightReqnrollFramework.StepDefinitions;
 [Binding]
 public class InventoryStepDef : BasePage
 {
-    private readonly PageFactory _factory;
-    private readonly InventoryPage _inventoryPage;
-    private readonly ProductPage _productPage;
-    private readonly CheckoutPage _checkoutPage;
-    private readonly CheckoutOverviewPage _checkoutOverviewPage;
-    private readonly CheckoutCompletePage _checkoutCompletePage;
     private float totalPrice = 0.0f;
     public InventoryStepDef(ScenarioContext scenarioContext) : base(scenarioContext)
     {
-        _factory = new PageFactory(scenarioContext);
-        _inventoryPage = _factory.GetPage<InventoryPage>();
-        _productPage = _factory.GetPage<ProductPage>();
-        _checkoutPage = _factory.GetPage<CheckoutPage>();
-        _checkoutOverviewPage = _factory.GetPage<CheckoutOverviewPage>();
-        _checkoutCompletePage = _factory.GetPage<CheckoutCompletePage>();
+       
     }
 
     [When(@"I add following item to the cart")]
@@ -33,8 +22,8 @@ public class InventoryStepDef : BasePage
 
         foreach (var item in items)
         {
-            _productPage.AddProductToCartAsync(item.ItemName).GetAwaiter().GetResult();
-            var itemPrice = _productPage.GetProductPriceAsync(item.ItemName).GetAwaiter().GetResult();
+            ProductPage.AddProductToCartAsync(item.ItemName).GetAwaiter().GetResult();
+            var itemPrice = ProductPage.GetProductPriceAsync(item.ItemName).GetAwaiter().GetResult();
             totalPrice += float.Parse(itemPrice);
 
         }
@@ -45,7 +34,7 @@ public class InventoryStepDef : BasePage
     [Then(@"I navigate to the cart")]
     public void ThenInavigatetothecart()
     {
-        _productPage.NavigateToCartAsync().GetAwaiter().GetResult();
+        ProductPage.NavigateToCartAsync().GetAwaiter().GetResult();
     }
 
 
@@ -56,9 +45,9 @@ public class InventoryStepDef : BasePage
 
         foreach (var item in items)
         {
-            bool isProductVisible = _inventoryPage.isProductInInventoryAsync(item.ItemName).GetAwaiter().GetResult();
+            bool isProductVisible = InventoryPage.isProductInInventoryAsync(item.ItemName).GetAwaiter().GetResult();
             Assert.That(isProductVisible, Is.True, $"Product '{item.ItemName}' is not visible in the inventory.");
-            float productPrice = _inventoryPage.GetProductPriceAsync(item.ItemName).GetAwaiter().GetResult();
+            float productPrice = InventoryPage.GetProductPriceAsync(item.ItemName).GetAwaiter().GetResult();
             Assert.That(productPrice, Is.EqualTo(item.ItemPrice), $"Product price for '{item.ItemName}' does not match. Expected: {item.ItemPrice}, Actual: {productPrice}");
         }
     }
@@ -67,14 +56,14 @@ public class InventoryStepDef : BasePage
     [Then(@"I clicked on the checkout button")]
     public void ThenIclickedonthecheckoutbutton()
     {
-        _inventoryPage.ClickCheckoutAsync().GetAwaiter().GetResult();
+        InventoryPage.ClickCheckoutAsync().GetAwaiter().GetResult();
     }
 
 
     [Then(@"I should be redirected to the checkout page")]
     public void ThenIshouldberedirectedtothecheckoutpage()
     {
-        bool isCheckedOutPage = _checkoutPage.IsCheckoutTitleVisibleAsync().GetAwaiter().GetResult();
+        bool isCheckedOutPage = CheckoutPage.IsCheckoutTitleVisibleAsync().GetAwaiter().GetResult();
         Assert.That(isCheckedOutPage, Is.True, "Checkout page is not loaded after clicking checkout button.");
     }
 
@@ -85,7 +74,7 @@ public class InventoryStepDef : BasePage
         //CrateInstance only fetch first row of the table
         //If you want to fetch all rows, use CreateSet<T>() method
         var details = table.CreateInstance<CheckoutDetails>();
-        _checkoutPage.FillCheckoutFormAsync(details.FirstName, details.LastName, details.PostalCode).GetAwaiter().GetResult();
+        CheckoutPage.FillCheckoutFormAsync(details.FirstName, details.LastName, details.PostalCode).GetAwaiter().GetResult();
 
     }
 
@@ -93,14 +82,14 @@ public class InventoryStepDef : BasePage
     [When(@"I click on the continue button")]
     public void WhenIclickonthecontinuebutton()
     {
-        _checkoutPage.ClickContinueButtonAsync().GetAwaiter().GetResult();
+        CheckoutPage.ClickContinueButtonAsync().GetAwaiter().GetResult();
     }
 
 
     [Then(@"I should be redirected to the overview page")]
     public void ThenIshouldberedirectedtotheoverviewpage()
     {
-        _checkoutOverviewPage.IsCheckoutOverviewTitleVisibleAsync().GetAwaiter().GetResult();
+        CheckoutOverviewPage.IsCheckoutOverviewTitleVisibleAsync().GetAwaiter().GetResult();
     }
 
 
@@ -111,8 +100,8 @@ public class InventoryStepDef : BasePage
 
         foreach (var item in items)
         {
-            _checkoutOverviewPage.IsProductInCheckoutOverviewAsync(item.ItemName).GetAwaiter().GetResult();
-            float productPrice = _checkoutOverviewPage.GetProductPriceAsync(item.ItemName).GetAwaiter().GetResult();
+            CheckoutOverviewPage.IsProductInCheckoutOverviewAsync(item.ItemName).GetAwaiter().GetResult();
+            float productPrice = CheckoutOverviewPage.GetProductPriceAsync(item.ItemName).GetAwaiter().GetResult();
             Assert.That(productPrice, Is.EqualTo(item.ItemPrice), $"Product price for '{item.ItemName}' does not match. Expected: {item.ItemPrice}, Actual: {productPrice}");
         }
     }
@@ -122,7 +111,7 @@ public class InventoryStepDef : BasePage
     public void ThenTheItemtotalshouldbe(float expectedItemTotal)
     {
 
-        float actualItemTotal = _checkoutOverviewPage.GetItemTotalAsync().GetAwaiter().GetResult();
+        float actualItemTotal = CheckoutOverviewPage.GetItemTotalAsync().GetAwaiter().GetResult();
         Assert.That(actualItemTotal, Is.EqualTo(expectedItemTotal), $"Expected Item Total: {expectedItemTotal}, Actual Item Total: {actualItemTotal}");
     }
 
@@ -130,14 +119,14 @@ public class InventoryStepDef : BasePage
     [Then(@"The Tax should be {float}")]
     public void ThenTheTaxshouldbe(float expectedTax)
     {
-        float actualTax = _checkoutOverviewPage.GetTaxAsync().GetAwaiter().GetResult();
+        float actualTax = CheckoutOverviewPage.GetTaxAsync().GetAwaiter().GetResult();
         Assert.That(actualTax, Is.EqualTo(expectedTax), $"Expected Tax: {expectedTax}, Actual Tax: {actualTax}");
     }
 
     [Then(@"The Total should be {float}")]
     public void ThenTheTotalshouldbe(float expectedGrandTotal)
     {
-        float actualTotal = _checkoutOverviewPage.GetTotalAsync().GetAwaiter().GetResult();
+        float actualTotal = CheckoutOverviewPage.GetTotalAsync().GetAwaiter().GetResult();
         Assert.That(actualTotal, Is.EqualTo(expectedGrandTotal), $"Expected Total: {expectedGrandTotal}, Actual Total: {actualTotal}");
     }
 
@@ -145,20 +134,20 @@ public class InventoryStepDef : BasePage
     [When(@"I click on the finish button")]
     public void WhenIclickonthefinishbutton()
     {
-        _checkoutOverviewPage.ClickFinishButtonAsync().GetAwaiter().GetResult();
+        CheckoutOverviewPage.ClickFinishButtonAsync().GetAwaiter().GetResult();
     }
 
     [Then(@"I should see the confirmation message ""(.*)""")]
     public void ThenIshouldseetheconfirmationmessage(string args1)
     {
-        _checkoutCompletePage.IsCheckoutCompleteMessageVisibleAsync().GetAwaiter().GetResult();
+        CheckoutCompletePage.IsCheckoutCompleteMessageVisibleAsync().GetAwaiter().GetResult();
 
     }
 
     [When(@"I click on the back home button")]
     public void WhenIclickonthebackhomebutton()
     {
-        _checkoutCompletePage.ClickBackHomeButtonAsync().GetAwaiter().GetResult();
+        CheckoutCompletePage.ClickBackHomeButtonAsync().GetAwaiter().GetResult();
     }
 
 
